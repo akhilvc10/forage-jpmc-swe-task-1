49791596adbd531152f1fd47fabdb18ceeb58545
Akhil VC <akhilvc10@gmail.com>
2023-09-04 17:54:22 +0530

    Fix client code

--- client_test.py
+++ client_test.py
@@ -1,25 +1,44 @@
 import unittest
-from client3 import getDataPoint
+from unittest.mock import patch, Mock
+import json
+import io
+from client3 import getDataPoint, getRatio  # Replace with your code file name
 
-class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+class TestFinancialMarketSimulation(unittest.TestCase):
+    @patch('urllib.request.urlopen')
+    def test_getDataPoint(self, mock_urlopen):
+        # Mock the response from the server
+        mock_response = io.StringIO(json.dumps([
+            {
+                'stock': 'ABC',
+                'top_bid': {'price': '100'},
+                'top_ask': {'price': '102'}
+            },
+            {
+                'stock': 'DEF',
+                'top_bid': {'price': '50'},
+                'top_ask': {'price': '52'}
+            }
+        ]))
+        mock_urlopen.return_value = Mock()
+        mock_urlopen.return_value.read.return_value = mock_response.getvalue()
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+        # Call the function and check the output
+        result = getDataPoint({
+            'stock': 'ABC',
+            'top_bid': {'price': '100'},
+            'top_ask': {'price': '102'}
+        })
+        self.assertEqual(result, ('ABC', 100.0, 102.0, 101.0))
 
+    def test_getRatio(self):
+        # Test when price_b is not zero
+        ratio = getRatio(100.0, 50.0)
+        self.assertEqual(ratio, 2.0)
 
-  """ ------------ Add more unit tests ------------ """
-
-
+        # Test when price_b is zero (should handle this case gracefully)
+        ratio = getRatio(100.0, 0)
+        self.assertIsNone(ratio)
 
 if __name__ == '__main__':
     unittest.main()
